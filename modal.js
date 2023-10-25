b26575e7b918bd34810d0274dcb3aef6922c6072
abbabashir <abbabashir516@gmail.com>
2023-10-25 23:40:24 +0100

    some changes

--- modal.js
+++ modal.js
@@ -1,14 +1,58 @@
 let modal = document.querySelector(".modal");
-      let trigger = document.querySelector(".example");
-      let closeButton = document.querySelector(".close-button");
-      function toggleModal() {
-        modal.classList.toggle("show-modal");
-      }
-      function windowOnClick(event) {
-        if(event.target === modal) {
-          toggleModal();
-        }
-      }
-      trigger.addEventListener("click", toggleModal);
-      closeButton.addEventListener("click", toggleModal);
-      window.addEventListener("click", windowOnClick);
+let trigger = document.querySelector(".new");
+let closeButton = document.querySelector(".close-button");
+function toggleModal() {
+  modal.classList.toggle("show-modal");
+}
+function windowOnClick(event) {
+  if(event.target === modal) {
+    toggleModal();
+  }
+}
+trigger.addEventListener("click", toggleModal);
+closeButton.addEventListener("click", toggleModal);
+window.addEventListener("click", windowOnClick);
+
+setInterval(showTime, 1000);
+ 
+// Defining showTime funcion
+function showTime() {
+    // Getting current time and date
+    let time = new Date();
+    let hour = time.getHours();
+    let min = time.getMinutes();
+    let sec = time.getSeconds();
+    am_pm = "AM";
+ 
+    // Setting time for 12 Hrs format
+    if (hour >= 12) {
+        if (hour > 12) hour -= 12;
+        am_pm = "PM";
+    } else if (hour == 0) {
+        hr = 12;
+        am_pm = "AM";
+    }
+ 
+    hour =
+        hour < 10 ? "0" + hour : hour;
+    min = min < 10 ? "0" + min : min;
+    sec = sec < 10 ? "0" + sec : sec;
+ 
+    let currentTime =
+        hour +
+        ":" +
+        min +
+        ":" +
+        sec +
+        am_pm;
+ 
+    // Displaying the time
+    document.querySelector(".time").innerHTML = currentTime;
+    // document.getElementById(
+    //     "clock"
+    // ).innerHTML = currentTime;
+}
+ 
+showTime();
+
+
